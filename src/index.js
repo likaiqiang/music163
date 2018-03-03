9cf662d6ccc0892b4cab520cd2477ca114c612e8
likaiqiang <921085426@qq.com>
2018-03-03 19:17:58 +0800

    add react && react-dom 16

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
+import Context from './context.js'
 import './index.css'
 import App from './App'
 import Playing from './pages/playing/playing'
@@ -8,11 +9,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Router>
-        <Switch>
-            <Route path='/app' component={App}></Route>
-            <Route path='/playing' component={Playing}></Route>
-            <Redirect from='/' to='/app/rec'></Redirect>
-        </Switch>
+        <Context.Provider value={{music:null}}>
+            <Switch>
+                <Route path='/app' component={App}></Route>
+                <Route path='/playing' component={Playing}></Route>
+                <Redirect from='/' to='/app/rec'></Redirect>
+            </Switch>
+        </Context.Provider>
     </Router>, 
     document.getElementById('root')
 )
