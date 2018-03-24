d01269109d0dd042c0afe61f43bdb69a23887f61
likaiqiang <921085426@qq.com>
2018-03-24 13:08:29 +0800

    search.js

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,10 +1,39 @@
 import React,{Component} from "react"
-
+import "./search.scss"
 class Search extends Component{
+    constructor(){
+        super(...arguments)
+        this.state = {
+            value:''
+        }
+        this.inputChange = this.inputChange.bind(this)
+        this.clear = this.clear.bind(this)
+    }
     render(){
         return (
-            <div>search</div>
+            <div>
+                <form className="searchWrap">
+                    <i className="searchIcon iconfont icon-search"></i>
+                    <input type="text" value={this.state.value} onChange={this.inputChange}/>
+                    {
+                        this.state.value.trim().length>0?<i className="searchClear iconfont icon-close" onClick={this.clear}></i>:null
+                    }
+                </form>
+                <ul>
+                    <li>当那一天来临</li>
+                </ul>
+            </div>
         )
+    }
+    inputChange(e){
+        this.setState({
+            value:e.target.value
+        })
+    }
+    clear(){
+        this.setState({
+            value:''
+        })
     }
 }
 
